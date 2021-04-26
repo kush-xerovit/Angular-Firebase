04f3a547b81bbe1d0c018465ec4109af6de4e96e
kush-xerovit <kush@xerovit.io>
2021-04-26 15:07:38 +0630

    updated on app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserListComponent } from 'src/app/pages/user/user-list/user-list.component';
+
 
 const routes: Routes = [
+
+  {
+    path: '',
+    component: UserListComponent,
+  },
   {
     path: 'user',
     loadChildren: () =>
